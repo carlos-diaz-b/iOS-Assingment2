3e84c293158e6b0c398c88ee75d497e7ca12ff90
Carlos Diaz <carlosandresdiazbravo@Carloss-MacBook-Pro.local>
2020-04-20 13:06:11 -0700

    assignment2

---
+++ AutoLayoutStoryboard/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AutoLayoutStoryboard
+//
+//  Created by Carlos andres Diaz bravo  on 2020-04-20.
+//  Copyright © 2020 Carlos andres Diaz bravo . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+     
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet var purplebox: UIView!
+    
+}
+
